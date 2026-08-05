a03cd1e7a51cddff7cd8c2876d1127808ef10e6a
John Ryan <johnryan6465@gmail.com>
2026-08-05 00:51:53 -0500

    Edit codealong & lecture 4

--- codealongs/CodeAlong4.jl
+++ codealongs/CodeAlong4.jl
@@ -13,10 +13,6 @@ using Optim, Interpolations, Plots, Polynomials
 ################################################################################
 
 Runge(x) = 1 /(1 + 25*x^2)
-function Runge_2(x)
-    val = 1/(1+25*x^2)
-    return val
-end
 x_fine = collect(-1.0:0.01:1.0)
 x_coarse = collect(range(-1.0, length = 6, stop = 1.0))
 plot(x_fine, Runge.(x_fine))
@@ -133,6 +129,156 @@ y_uneven = log.(x_uneven)
 interp_y = cubic_spline_interpolation(x_uneven, y_uneven) #Error because x is not evenly spaced. 
 
 
+################################################################################
+# Quadrature
+################################################################################
+
+using FastGaussQuadrature, LinearAlgebra
+
+#Gauss-Legendre Quadrature for approximating integral between -1 and 1
+#Works well with n nodes if function is well approximated by polynomial of degree n
+#Get nodes x and weights w
+x, w = gausslegendre(3)
+
+#Function F
+F(x) = x^4 - 3x^3 - 4x^2
+#Derivative of function f
+f(x) = 4x^3 - 9x^2 - 8x
+#Calculate intergral appoximation
+I = w ⋅ f.(x)
+#Calculate true value
+F(1) - F(-1)
+
+
+# Gauss-Hermite Quadrature for approximating integral of function g(x) = f(x)*exp(-x^2)
+# Again, works well with n nodes if f is well approximated by polynomial of degree n
+# Here we will approximate the expection E[f(x)] when x is distributed according to standard normal
+
+#Get nodes and weights
+x,w = gausshermite(9)
+#Change of variable: https://en.wikipedia.org/wiki/Gauss-Hermite_quadrature
+g_tilde(x,f) = f(sqrt(2)x)/sqrt(π)
+
+#Expectation approximation
+w ⋅ g_tilde.(x,f)
+
+#Monte Carlo Simulation Approximation
+using Distributions
+val = 0
+N = 1_000_000
+for i = 1:N
+    val += f(rand(Normal()))/N
+end
+val
+
+################################################################################
+# Growth, Revisited: Interpolation + Optimization Instead of Grid Search
+################################################################################
+
+# Lecture 2 solved this model by grid search: at every k, loop over every k'
+# on the grid and keep the best one. That works, but needs a fine grid to be
+# accurate, and "the best k' on the grid" is never quite the true optimum.
+# Now that we can interpolate V between grid points (this lecture) and
+# optimize continuously over k' (Lecture 3), we can do much better with far
+# fewer grid points.
+
+# We use Julia's built-in @kwdef macro to define default values in our structs.
+# (Older code often uses the Parameters.jl package and its @with_kw macro for this;
+#  @kwdef is built in and does the same job.)
+
+### Struct for our model paramters
+@kwdef struct ModelParameters
+
+    β::Float64 = 0.99 #We can define default values because of the @kwdef macro.
+    δ::Float64 = 0.025
+    α::Float64 = 0.36
+
+    k_grid::Vector{Float64}=collect(range(0.1, length = 100, stop = 45.0)) #### Far fewer grid points than grid search!
+    N_k::Int64 = length(k_grid)
+
+    tol::Float64 = 10^-5
+    max_iter::Int64 = 10^4
+
+end
+
+
+struct ModelSolutions
+
+    V::Vector{Float64}
+    kp::Vector{Float64}
+
+end
+
+function initialize()
+    para = ModelParameters()
+    V = zeros(para.N_k)
+    kp = zeros(para.N_k)
+    sols = ModelSolutions(V, kp)
+    return para, sols
+end
+
+### Bellman operator
+function bellman(para, sols)
+    (; β, δ, α, k_grid) = para
+    (; V, kp) = sols
+
+    V_next = zero(V)
+    kp_next = zero(kp)
+
+    #Interpolate value function for continuation value
+    V_interp = linear_interpolation(k_grid, V, extrapolation_bc = Interpolations.Flat())
+
+    for (i_k, k) = enumerate(k_grid)
+
+        budget = k^α + (1-δ)*k
+
+        #Replace grid search with box constrained optimization!
+        obj(kp) = -(log(budget-kp) + β*V_interp(kp))
+        opt = optimize(obj, 0.0, budget - 1e-8)
+
+        V_next[i_k] = -opt.minimum
+        kp_next[i_k] = opt.minimizer
+
+    end
+
+    return V_next, kp_next
+
+end
+
+
+
+### Solve model
+function solve_model!(para, sols)
+    (; tol, max_iter) = para
+    (; V, kp) = sols
+
+    V_next = zero(V)
+    kp_next = zero(kp)
+    max_diff = tol + 10.0
+    n = 0
+    while max_diff > tol && n < max_iter
+        n +=1
+        V_next, kp_next = bellman(para,sols)
+
+        max_diff = maximum(abs.(V_next - V))
+        V .= V_next
+        kp .= kp_next
+
+        @show n, max_diff
+
+    end
+end
+
+para, sols = initialize();
+
+#So fast and just as accurate!
+@time solve_model!(para,sols)
+
+plot(para.k_grid, sols.V)
+plot(para.k_grid, sols.kp)
+plot!(collect(0:45), collect(0:45))
+
+
 ################################################################
 # Bilinear interpolation
 ################################################################
@@ -172,46 +318,7 @@ contourf(grid_fine, grid_fine, (z_interp - z_fine)./z_fine) #Percent Difference
 surface(grid_fine,grid_fine,z_fine)
 surface!(grid_fine,grid_fine, z_interp)
 
-################################################################################
-# Quadrature
-################################################################################
 
-using FastGaussQuadrature, LinearAlgebra
-
-#Gauss-Legendre Quadrature for approximating integral between -1 and 1
-#Works well with n nodes if function is well approximated by polynomial of degree n
-#Get nodes x and weights w
-x, w = gausslegendre(3)
-
-#Function F
-F(x) = x^4 - 3x^3 - 4x^2
-#Derivative of function f
-f(x) = 4x^3 - 9x^2 - 8x
-#Calculate intergral appoximation
-I = sum(w .* f.(x))
-#Calculate true value
-F(1) - F(-1)
-
-
-# Gauss-Hermite Quadrature for approximating integral of function g(x) = f(x)*exp(-x^2)
-# Again, works well with n nodes if f is well approximated by polynomial of degree n
-# Here we will approximate the expection E[f(x)] when x is distributed according to standard normal
-
-#Get nodes and weights
-x,w = gausshermite(9)
-#Change of variable: https://en.wikipedia.org/wiki/Gauss-Hermite_quadrature
-g_tilde(x,f) = f(sqrt(2)x)/sqrt(π)
-
-#Expectation approximation
-sum(w.*g_tilde.(x,f))
-
-#Monte Carlo Simulation Approximation
-val = 0
-N = 1000000
-for i = 1:N
-    val += f(rand(Normal()))/N
-end
-val
 
 
 
